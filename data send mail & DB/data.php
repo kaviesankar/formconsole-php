9e191dd67b1137b66a7889f1ef0c51f2f75cd976
Kavisankar Eswaran <84067576+kavisankar-eswaran@users.noreply.github.com>
2021-08-06 19:18:25 +0530

    Create data.php

---
+++ data send mail & DB/data.php
@@ -0,0 +1,71 @@
+<?php
+
+$uname = $_POST['uname'];
+$cname = $_POST['cname'];
+$mob = $_POST['mob'];
+$email  = $_POST['email'];
+$cadd = $_POST['cadd'];
+$wname = $_POST['wname'];
+$msg = $_POST['msg'];
+
+$to = "payment@foefox.com";
+$subject = "Invoice From Foefox Payments";
+$txt ="Name = ". $uname . "\r\n  Company Name = " . $cname . "\r\n  Mobile Number = " . $mob . "\r\n  Company Address = " . $cadd . "\r\n  Email = " . $email . "\r\n Website Name =" . $wname . "\r\n Message =" . $msg;
+$headers = "From: $email " . "\r\n" .
+"CC: Payment Invoices";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+
+
+
+if (!empty($uname) || !empty($cname) || !empty($mob) || !empty($email) || !empty($cadd) || !empty($wname) || !empty($msg) )
+{
+
+$host = "localhost";
+$dbusername = "foef14_pay";
+$dbpassword = "mdoindia1@";
+$dbname = "foef14_pay";
+
+
+
+// Create connection
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+
+if (mysqli_connect_error()){
+  die('Connect Error ('. mysqli_connect_errno() .') '
+    . mysqli_connect_error());
+}
+else{
+  $SELECT = "SELECT email From invoice Where email = ? Limit 10";
+  $INSERT = "INSERT Into invoice (uname , cname, mob, email ,cadd, wname, msg )values(?,?,?,?,?,?,?)";
+
+//Prepare statement
+     $stmt = $conn->prepare($SELECT);
+     $stmt->bind_param("s", $email);
+     $stmt->execute();
+     $stmt->bind_result($email);
+     $stmt->store_result();
+     $rnum = $stmt->num_rows;
+
+     //checking username
+      if ($rnum==0) {
+      $stmt->close();
+      $stmt = $conn->prepare($INSERT);
+      $stmt->bind_param("sssssss", $uname,$cname,$mob,$email,$cadd,$wname,$msg);
+      $stmt->execute();
+      echo "New record inserted sucessfully";
+     } else {
+      echo "Someone already register using this email";
+     }
+     $stmt->close();
+     $conn->close();
+    }
+} else {
+ echo "All field are required";
+ die();
+}
+
+//redirect
+header("Location:https://pages.razorpay.com/foefox-economy");
+?>
