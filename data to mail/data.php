c43e00bb2dc5eaa48afbc46f3de3ea7a5bfdfa81
Kavisankar Eswaran <84067576+kavisankar-eswaran@users.noreply.github.com>
2021-08-10 09:58:34 +0530

    Update data.php

--- data to mail/data.php
+++ data to mail/data.php
@@ -1,1 +1,19 @@
-
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$qualification= $_POST['qualification'];
+$role= $_POST['role'];
+$file = $_POST['file'];
+//your mail
+$to = "xxx@yyy.com";
+$subject = "Sub";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n qualification =" . $qualification . "\r\n role =" . $role "\r\n file =" . $file;
+$headers = "From: $email " . "\r\n" .
+"CC: xxx@yyy.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:your redirect site");
+?>
