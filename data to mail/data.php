7df1778ee4bcdcbf8986b5f4ba81b47ca7519909
Kavisankar Eswaran <84067576+kavisankar-eswaran@users.noreply.github.com>
2021-10-17 19:59:07 +0530

    Update data.php

--- data to mail/data.php
+++ data to mail/data.php
@@ -9,7 +9,7 @@ $file = $_POST['file'];
 $to = "xxx@yyy.com";
 $subject = "Sub";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n qualification =" . $qualification . "\r\n role =" . $role "\r\n file =" . $file;
-$headers = "From: $email " . "\r\n" .
+$headers = "From: $email " . "\r\n" . 
 "CC: xxx@yyy.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
